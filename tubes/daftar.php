9315dcbc6b6352585ca55f9e29990199bd677e2b
naifah13 <sitinaifah473@gmail.com>
2025-12-07 14:08:18 +0700

    Add files via upload

---
+++ tubes/daftar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Daftar</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+<section class="auth-section">
+    <div class="auth-card">
+        <h2>Daftar</h2>
+
+        <input class="input" type="text" placeholder="Nama Lengkap" style="margin-bottom: 15px;">
+        <input class="input" type="text" placeholder="Username" style="margin-bottom: 15px;">
+        <input class="input" type="email" placeholder="Email" style="margin-bottom: 15px;">
+        <input class="input" type="password" placeholder="Password" style="margin-bottom: 15px;">
+
+        <a href="home.php" class="btn btn-primary btn-lg auth-btn">Daftar</a>
+
+        <!-- Tambahan -->
+        <p style="margin-top: 20px; font-size: 14px; text-align:center;">
+            Sudah memiliki akun?
+            <a href="login.php" style="color: #007bff; text-decoration: none;">Klik di sini</a>
+        </p>
+    </div>
+</section>
+
+</body>
+</html>
