5091b855478f7cef6cb7521ad6cdb0f5040916a1
naifah13 <sitinaifah473@gmail.com>
2025-12-07 14:00:34 +0700

    Update login.php

--- tubes/login.php
+++ tubes/login.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
@@ -9,15 +10,15 @@
 
 <body>
 
-<section class="auth-section">
-    <div class="auth-card">
-        <h2>Login</h2>
-        <input class="input" type="email" placeholder="Email">
-        <input class="input" type="password" placeholder="Password">
-        <a href="home.php" class="btn btn-primary btn-lg auth-btn">Login</a>
-    </div>
-</section>
+    <section class="auth-section">
+        <div class="auth-card">
+            <h2>Login</h2>
+            <input class="input" type="email" placeholder="Email" style="margin-bottom: 15px;">
+            <input class="input" type="password" placeholder="Password">
+            <a href="home.php" class="btn btn-primary btn-lg auth-btn">Login</a>
+        </div>
+    </section>
 
 </body>
-</html>
 
+</html>
