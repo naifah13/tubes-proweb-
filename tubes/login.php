cba21e8d97f8b30eb7670d6deec0dd5b487572c1
naifah13 <sitinaifah473@gmail.com>
2025-12-14 15:55:17 +0700

    pake alert

--- tubes/login.php
+++ tubes/login.php
@@ -2,34 +2,32 @@
 session_start();
 require_once("koneksi.php");
 
-if(isset($_POST['btn-lg']))
-{
+if (isset($_POST['btn-lg'])) {
+
     $user_login = $_POST['username'];
     $pass_login = $_POST['password'];
 
     $sql = "SELECT * FROM data_pengguna WHERE username = '$user_login'";
-    $query = mysqli_query($koneksi,$sql);
+    $query = mysqli_query($koneksi, $sql);
 
-    if(!$query){
+    if (!$query) {
         die("Query gagal" . mysqli_error($koneksi));
     }
 
     $user = "";
     $pass = "";
 
-    while($row = mysqli_fetch_array($query)){
+    while ($row = mysqli_fetch_array($query)) {
         $user = $row['username'];
         $pass = $row['password'];
-        $nama = $row['nama_lengkap'];
-        $email = $row['email'];
     }
 
-    if($user_login == $user && password_verify($pass_login, $pass)){
+    if ($user_login == $user && password_verify($pass_login, $pass)) {
         $_SESSION['username'] = $user;
         header("Location: home.php");
         exit;
     } else {
-        echo "User atau Password Salah!";
+        $login_error = true;
     }
 }
 ?>
@@ -53,10 +51,12 @@ if(isset($_POST['btn-lg']))
         <h2>Login</h2>
 
         <form action="" method="POST">
-            <input class="input" type="text" name="username" placeholder="Username" style="margin-bottom: 15px;" required>
-            <input class="input" type="password" name="password" placeholder="Password" style="margin-bottom: 15px;" required>
+            <input class="input" type="text" name="username" placeholder="Username" required style="margin-bottom: 15px;">
+            <input class="input" type="password" name="password" placeholder="Password" required style="margin-bottom: 15px;">
 
-            <button type="submit" name="btn-lg" class="btn btn-primary btn-lg auth-btn">Login</button>
+            <button type="submit" name="btn-lg" class="btn btn-primary btn-lg auth-btn">
+                Login
+            </button>
         </form>
 
         <p style="margin-top: 20px; font-size: 14px; text-align:center;">
@@ -65,6 +65,12 @@ if(isset($_POST['btn-lg']))
         </p>
     </div>
 </section>
+
+<script>
+<?php if (isset($login_error)) : ?>
+    alert("Username atau Password salah!");
+<?php endif; ?>
+</script>
 
 </body>
 </html>
